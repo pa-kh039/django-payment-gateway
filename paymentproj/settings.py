e811b6947cbe2908bf77c9b6ddc3fde31024fa2d
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-05-05 22:51:36 +0530

    txt to py file

--- paymentproj/settings.py
+++ paymentproj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from '../../conf.txt' import key1, key2, dsk
+from conf import key1, key2, dsk
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
